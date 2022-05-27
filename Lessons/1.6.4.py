2872a73454ddd2a48006fa95f0132470641eccbb
dkartushin <dkartushin@tradingview.com>
2022-05-27 09:48:18 +0300

    Replaced depricated find_element_by_*

--- Lessons/1.6.4.py
+++ Lessons/1.6.4.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+from selenium.webdriver.common.by import By
 
 link = "https://suninjuly.github.io/simple_form_find_task.html"
 
@@ -11,15 +12,15 @@ try:
     browser = webdriver.Chrome(ChromeDriverManager().install(), options=option)
     browser.get(link)
 
-    input1 = browser.find_element_by_css_selector(".form-group:nth-child(1) input")
+    input1 = browser.find_element(By.CSS_SELECTOR, ".form-group:nth-child(1) input")
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_css_selector(".form-group:nth-child(2) input")
+    input2 = browser.find_element(By.CSS_SELECTOR, ".form-group:nth-child(2) input")
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_css_selector(".form-group:nth-child(3) input")
+    input3 = browser.find_element(By.CSS_SELECTOR, ".form-group:nth-child(3) input")
     input3.send_keys("Smolensk")
-    input4 = browser.find_element_by_css_selector(".form-group:nth-child(4) input")
+    input4 = browser.find_element(By.CSS_SELECTOR, ".form-group:nth-child(4) input")
     input4.send_keys("Russia")
-    button = browser.find_element_by_css_selector("button.btn")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
 finally:
--- Lessons/1.6.5.py
+++ Lessons/1.6.5.py
@@ -17,15 +17,15 @@ try:
     da_link = browser.find_element_by_partial_link_text(link_text)
     da_link.click()
 
-    input1 = browser.find_element_by_css_selector(".form-group:nth-child(1) input")
+    input1 = browser.find_element(By.CSS_SELECTOR, ".form-group:nth-child(1) input")
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_css_selector(".form-group:nth-child(2) input")
+    input2 = browser.find_element(By.CSS_SELECTOR, ".form-group:nth-child(2) input")
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_css_selector(".form-group:nth-child(3) input")
+    input3 = browser.find_element(By.CSS_SELECTOR, ".form-group:nth-child(3) input")
     input3.send_keys("Smolensk")
-    input4 = browser.find_element_by_css_selector(".form-group:nth-child(4) input")
+    input4 = browser.find_element(By.CSS_SELECTOR, ".form-group:nth-child(4) input")
     input4.send_keys("Russia")
-    button = browser.find_element_by_css_selector("button.btn")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
 finally:
