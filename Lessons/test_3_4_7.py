347f595886c14f3beaa3f9d11ad2d50238d2ed31
dkartushin <dkartushin@tradingview.com>
2022-06-06 15:13:01 +0300

    Added test_3_4_7

---
+++ Lessons/test_3_4_7.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+@pytest.fixture(scope="class")
+def prepare_faces():
+    print("^_^", "\n")
+    yield
+    print(":3", "\n")
+
+
+@pytest.fixture()
+def very_important_fixture():
+    print(":)", "\n")
+
+
+@pytest.fixture(autouse=True)
+def print_smiling_faces():
+    print(":-Р", "\n")
+
+
+class TestPrintSmilingFaces():
+    def test_first_smiling_faces(self, prepare_faces, very_important_fixture):
+        print('test_first_smiling_faces', "\n")
+
+    def test_second_smiling_faces(self, prepare_faces):
+        print('test_second_smiling_faces', "\n")
