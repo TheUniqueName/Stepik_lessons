f72c37a636ed6c42371cccd5c667cd4edee2928e
dkartushin <dkartushin@tradingview.com>
2022-06-14 15:58:25 +0300

    3_6_9 v0.9

--- 3.6.9/test_items.py
+++ 3.6.9/test_items.py
@@ -1,10 +1,12 @@
 from selenium.webdriver.common.by import By
 import time
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_login_link_pass(browser):
     browser.get(link)
-    time.sleep(30)
-    browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    time.sleep(5)
+    submit_button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+
+    assert submit_button, "There is no button"
